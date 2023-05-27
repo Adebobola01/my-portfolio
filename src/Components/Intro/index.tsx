123dbce236c00b0eba668f99453f12d0e14c5ec4
Adebobola01 <m.oyedunmade@gmail.com>
2023-05-27 20:56:21 +0100

    updated intro

--- src/Components/Intro/index.tsx
+++ src/Components/Intro/index.tsx
@@ -17,9 +17,11 @@ const Intro = () => {
     return (
         <section id="about" className={styles.about_me} >
             <blockquote>
-                HI, I'm Adebobola, A software developer with 2 years of experience With a strong background in
-                blockchain technology and decentralized applications (dapps).
-                <br/>
+                <p>Hi, my name is</p>
+                <h1>Adebobola Oyedunmade</h1>
+                    A software developer with 2 years of experience With a strong background in
+                    blockchain technology and decentralized applications (dapps).
+                {/* <br/>
                 <br/>
                 I possess an in-depth understanding of smart contracts and the Ethereum blockchain network, enabling me to build secure
                 decentralized applications. My expertise lies in developing frontend and backend solutions,
@@ -28,7 +30,7 @@ const Intro = () => {
                 <br/>
                 If you are looking to bring your ideas to life or need assistance in building a web3 dapp, I am here to help.
                 Let's collaborate and create impactful decentralized applications
-                that redefine the way we interact and transact in the digital world.
+                that redefine the way we interact and transact in the digital world. */}
             </blockquote>
             <Socials type="desktop"/>
         </section>
