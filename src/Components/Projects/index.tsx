de43369b23a04bb4fcbe28915d487a6c22bcf64f
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2023-07-16 13:58:34 +0100

    added forkify

--- src/Components/Projects/index.tsx
+++ src/Components/Projects/index.tsx
@@ -4,6 +4,7 @@ import Project from "./Project";
 import itaImg from "../../assets/images/dev-icon.jpg";
 import parodyImg from "../../assets/images/parody.jpg";
 import big3Img from "../../assets/images/big3 prev.jpg";
+import forkifyImg from "../../assets/images/forkify.png";
 
 const Projects = () => {
     const domRef: any = useRef();
@@ -19,13 +20,13 @@ const Projects = () => {
         observer.observe(domRef.current);
         return () =>observer.unobserve(domRef.current)
     }, [])
-    console.log(isVisible)
     return (
         <section className={`${styles.projects} ${ isVisible ? styles.isVisible : null}`} id="projects" ref={domRef}>
             <h2>My Projects</h2>
             <div className={styles.container}>
                 <Project name="Uniswap Parody" image={parodyImg} description="A UI clone of the swapping protocol uniswap" stacks={["React", "Typescript", "SCSS", ]} codeLink="https://github.com/Adebobola01/uniswap-parody" liveLink="https://uniswap-parody.vercel.app/" />
                 <Project name="big3 Marketplace" image={big3Img} description="A NFT marketPlace." stacks={["React", "Typescript", "SCSS", ]} codeLink="https://github.com/Adebobola01/big3-react" liveLink="https://big3.onrender.com/" />
+                <Project name="Forkify" image={forkifyImg} description="A recipe application" stacks={["HTML", "CSS", "JAVASCRIPT(MVP)"]} liveLink="https://forkify-bobola.netlify.app/" codeLink="https://github.com/Adebobola01/forkify"/>
             </div>
         </section>
     )
