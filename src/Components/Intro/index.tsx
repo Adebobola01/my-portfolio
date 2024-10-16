1cd0f81a4ff4d56a8bf7c2cad0e619666b9c9c2c
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2024-10-16 20:36:06 +0100

    feat: updated

--- src/Components/Intro/index.tsx
+++ src/Components/Intro/index.tsx
@@ -5,25 +5,25 @@ import devIcon from "../../assets/images/dev-icon.jpg";
 import Socials from "../socials";
 
 const Intro = () => {
+  const style = {
+    strokeDasharray: "9541.93, 9541.93",
+    strokeDashoffset: "7156",
+    transition: "fill 1s ease-in-out 0s",
+    stroke: "rgb(204, 204, 204)",
+    fill: "rgb(204, 204, 204)",
+  };
 
-    const style = {
-        strokeDasharray: "9541.93, 9541.93",
-        strokeDashoffset: "7156",
-        transition: "fill 1s ease-in-out 0s",
-        stroke: "rgb(204, 204, 204)",
-        fill: "rgb(204, 204, 204)",
-    }
-    
-    return (
-        <section className={styles.intro} >
-            <blockquote>
-                <span>Hi, my name is</span>
-                <h1>Adebobola Oyedunmade</h1>
-                <p>
-                    A software developer with 2 years of experience With a strong background in
-                    blockchain technologies and decentralized applications (dapps).
-                </p>
-                {/* <br/>
+  return (
+    <section className={styles.intro}>
+      <blockquote>
+        <span>Hi, my name is</span>
+        <h1>Adebobola Oyedunmade</h1>
+        <p>
+          A software developer with 3.5 years of experience With a strong
+          background in blockchain technologies and decentralized applications
+          (dapps).
+        </p>
+        {/* <br/>
                 <br/>
                 I possess an in-depth understanding of smart contracts and the Ethereum blockchain network, enabling me to build secure
                 decentralized applications. My expertise lies in developing frontend and backend solutions,
@@ -33,11 +33,13 @@ const Intro = () => {
                 If you are looking to bring your ideas to life or need assistance in building a web3 dapp, I am here to help.
                 Let's collaborate and create impactful decentralized applications
                 that redefine the way we interact and transact in the digital world. */}
-            </blockquote>
-            <a href="#contact-me" className={styles.intro_btn}>Contact Me</a> 
-            <Socials type="desktop"/>
-        </section>
-    )
-}
+      </blockquote>
+      <a href="#contact-me" className={styles.intro_btn}>
+        Contact Me
+      </a>
+      <Socials type="desktop" />
+    </section>
+  );
+};
 
 export default Intro;
--- src/Components/about/index.tsx
+++ src/Components/about/index.tsx
@@ -1,40 +1,50 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./about.module.scss";
 
 const About = (props: any) => {
-    const domRef: any = useRef();
-    const [isVisible, setVisible] = useState(false);
+  const domRef: any = useRef();
+  const [isVisible, setVisible] = useState(false);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(e => {
-                // console.log("intersecting", isVisible)
-                if (e.isIntersecting) {                    
-                    props.handler(false);
-                }
-                return setVisible(e.isIntersecting)
-            })
-        });
-        observer.observe(domRef.current);
-        return () => observer.unobserve(domRef.current)
-    }, [])
-    
-    return (
-        <section className={`${styles.about} ${ isVisible ? styles.isVisible : null}`} ref={domRef} id="about" >
-            <h2>About Me</h2>
-            <p>
-                I possess an in-depth understanding of smart contracts and the Ethereum blockchain network, enabling me to build secure
-                decentralized applications. My expertise lies in developing frontend and backend solutions,
-                integrating blockchain protocols, and implementing smart contract functionalities.
-                <br/>
-                <br/>
-                If you are looking to bring your ideas to life or need assistance in building a web3 dapp, I am here to help.
-                {/* Let's collaborate and create impactful decentralized applications
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((e) => {
+        // console.log("intersecting", isVisible)
+        if (e.isIntersecting) {
+          props.handler(false);
+        }
+        return setVisible(e.isIntersecting);
+      });
+    });
+    observer.observe(domRef.current);
+    return () => observer.unobserve(domRef.current);
+  }, []);
+
+  return (
+    <section
+      className={`${styles.about} ${isVisible ? styles.isVisible : null}`}
+      ref={domRef}
+      id="about"
+    >
+      <h2>About Me</h2>
+      <p>
+        I possess an in-depth understanding of smart contracts and the Ethereum
+        blockchain network, enabling me to build secure decentralized
+        applications. My expertise lies in developing frontend and backend
+        solutions, integrating blockchain protocols, and implementing smart
+        contract functionalities. Software developer with three and a half years
+        of experience developing and managing websites and web applications,
+        with an in-depth understanding of smart contracts and the Ethereum
+        blockchain network, enabling me to build secure decentralized
+        applications.
+        <br />
+        <br />
+        If you are looking to bring your ideas to life or need assistance in
+        building a web2 or web3 application, I am here to help.
+        {/* Let's collaborate and create impactful decentralized applications
                 that redefine the way we interact and transact in the digital world. */}
-            </p>
-        </section>
-    )
-}
-
+      </p>
+    </section>
+  );
+};
 
 export default About;
