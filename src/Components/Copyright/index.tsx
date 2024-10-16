2e126c25fdd37a8c4460e1ca9db7656b6929846a
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2024-10-16 20:39:20 +0100

    feat: updated

--- src/Components/Copyright/index.tsx
+++ src/Components/Copyright/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 
-
-
 const Copyright = () => {
-    const style = {
-        color: "white"
-    }
-    
-    return (
-        <p style={style}>© Adebobola Oyedunmade 2023. All rights reserved</p>
-    )
-}
+  const style = {
+    color: "white",
+  };
+
+  return <p style={style}>© Adebobola Oyedunmade 2024. All rights reserved</p>;
+};
 
 export default Copyright;
